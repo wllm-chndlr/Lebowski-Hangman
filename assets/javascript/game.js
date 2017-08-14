2dfaed0f22c56e6cf5322608f1c69cf468940110
Chandler <chndlr.wllm@gmail.com>
2017-08-14 15:55:16 -0500

    170814-1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 var allLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var words = ['syndrome', 'elastic', 'geometry', 'electricity', 'whisp', 'breakfast', "harassment", "illicit", "bonkers", "zoological"];
+var words = ['abide', 'bunny', 'beverage', 'dude', 'rug', 'nihilist', 'bowling', 'prince', 'housebroken', 'undies', 'sarsaparilla', 'strumpet', 'fascist', 'coitus', 'zesty', 'marmot', 'walrus', 'cleft', 'aggression', 'pacifism', 'vagrant', 'moonless'];
 var currentWord = words[Math.floor(Math.random() * words.length)];
 var guessWord = ""; //empty string to hold guesses
-var guesses = 10; //remaining guesses
+var guesses = currentWord.length+3; //remaining guesses
 var lettersGuessed = [];
 var wins = 0;
 var losses = 0;
@@ -11,13 +11,16 @@ for (var i = 0; i < currentWord.length; i++) {
   guessWord = guessWord + "_";
 }
 
-console.log(currentWord);
+console.log(currentWord); //hint
 
 document.querySelector('#game').innerHTML = 
-"Current Word:<br>" + guessWord +
+"Current word:<br>" + guessWord +
 "<br><br> Number of guesses:<br>" + guesses +
 "<br><br> Letters already guessed:<br>" + lettersGuessed +
-"<br><br>Wins:<br>" + wins +
+"<br>";
+
+document.querySelector('#stats').innerHTML = 
+"Wins:<br>" + wins +
 "<br><br>Losses:<br>" + losses;
 
 function swapLetter(str, i, userInput) {
@@ -35,13 +38,23 @@ function reset() {
   }
   guesses = 10;
   lettersGuessed = [];
+  console.log(currentWord);
 }
-
 
 document.onkeyup = function(event) {
 
   var userInput = event.key;
   // var inWord = false;
+
+  if ((allLetters.includes(userInput)) === false) {
+    alert("Please select a letter from a-z");
+  }
+
+  if (lettersGuessed.indexOf(userInput) != -1) {
+    return("You already guessed that one!");
+  }
+
+  else {
 
   for (var i = 0; i < currentWord.length; i++) {
     
@@ -56,27 +69,34 @@ document.onkeyup = function(event) {
       lettersGuessed.push(userInput);
       guesses--;
     }
+  }
+
 
     if (guesses < 1) {
-      alert("You lose, sucka!");
+      alert("You human paraquat!");
       losses++;
       reset();
     }
 
     if (guessWord.indexOf("_") === -1) {
-      alert("You win!");
+      alert("The Dude abides!");
       wins++;
       reset();
     }
 
-
   var html = 
-  "Current Word:<br>" + guessWord +
+  "Current word:<br>" + guessWord +
   "<br><br> Number of guesses:<br>" + guesses +
-  "<br><br> Letters already guessed:<br>" + lettersGuessed +
-  "<br><br>Wins:<br>" + wins +
-  "<br><br>Losses:<br>" + losses;
+  "<br><br> Letters already guessed:<br>" + lettersGuessed;
+  // "<br><br>Wins:<br>" + wins +
+  // "<br><br>Losses:<br>" + losses;
   document.querySelector('#game').innerHTML = html;
+
+  var html =
+  "Wins:<br>" + wins +
+  "<br><br>Losses:<br>" + losses +
+  "<br>";
+  document.querySelector('#stats').innerHTML = html;
 
 }
 
